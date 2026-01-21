71b74533fe9b1c09e42be991c603c6b2786bc6c6
stucowebsolutions <stucowebsolutions@gmail.com>
2026-01-21 03:26:41 -0500

    Remove size and servings from fastBitesMenu
    Removed size and servings display from menu items.

--- trattoriadinapoli/testing/fastBitesMenu.js
+++ trattoriadinapoli/testing/fastBitesMenu.js
@@ -38,10 +38,6 @@ function renderFastBitesItem(item) {
       <span class="menu-item-price">${formatPrice(item.price)}</span>
     </div>
 
-    ${item.sizeLabel ? `<div class="menu-item-size">${item.sizeLabel}</div>` : ""}
-    ${item.servings.small || item.servings.large
-      ? `<div class="menu-item-servings">${formatServings(item.servings)}</div>`
-      : ""}
     ${item.choice ? `<div class="menu-item-choice">${item.choice}</div>` : ""}
     ${item.description
       ? `<div class="menu-item-description">${item.description}</div>`
